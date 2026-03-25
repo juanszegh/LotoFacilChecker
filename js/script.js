57134746a75f51cbb2f38b5844bcc1ecae3e064e
juanszegh <juan.ferreira11@etec.sp.gov.br>
2026-03-25 18:19:20 -0300

    number select

--- js/script.js
+++ js/script.js
@@ -2,10 +2,12 @@ import { $ } from "./util.js"
 const API_URL = "https://servicebus2.caixa.gov.br/portaldeloterias/api/lotofacil/"
 let jogo_atual = null
 let numeros_map = {}
-
+let concurso_atual = -1
+let ultimo_concurso = 0
 
 document.addEventListener('DOMContentLoaded', async () => {
     await init()
+    iniciarBotoesConcurso()
     $("#btn_atualizar").addEventListener('click', async () =>{
         await init()
     })
@@ -13,10 +15,23 @@ document.addEventListener('DOMContentLoaded', async () => {
 
 
 
-async function init()
+async function init(concurso = -1)
 {
-
-    jogo_atual = await (await fetch(API_URL)).json()
+    numeros_map = {}
+    let url_final = API_URL
+    if (concurso !== -1)
+    {
+        concurso = Math.min(Math.max(concurso,1),ultimo_concurso)    
+        url_final += concurso
+    }
+       
+    
+    jogo_atual = await (await fetch(url_final)).json()
+    if (concurso === -1)
+    {
+        ultimo_concurso = jogo_atual.numero
+    }
+    concurso_atual = jogo_atual.numero
     $("#data_jogo").textContent = jogo_atual.dataApuracao
     $("#numeros_jogo").innerHTML = ""
     jogo_atual.listaDezenas.forEach(n => {
@@ -44,10 +59,30 @@ async function init()
         div_jogos.appendChild(div_jogo_atual)
         div_jogos.innerHTML += `<p class="acertos">Acertos: ${acertos}</p>`
     })
-    
+    $("#numero_concurso").value = concurso_atual
 }
-
-
+function iniciarBotoesConcurso()
+{
+    const botoes = {
+        voltar_tudo: [$("#botoes_concurso > .voltar_tudo"), -ultimo_concurso],
+        voltar_um: [$("#botoes_concurso > .voltar_um"), -1],
+        avancar_um: [$("#botoes_concurso > .avancar_um"), 1],
+        avancar_tudo: [$("#botoes_concurso > .avancar_tudo"),ultimo_concurso],
+    }
+    Object.values(botoes).forEach(b => {
+        b[0].addEventListener('click', async () => {
+            await mudarConcurso(b[1]) 
+        })
+    })
+    const input = $("#botoes_concurso > input")
+    input.addEventListener('blur', async () => {
+        await init(input.value)
+    })
+}
+async function mudarConcurso(step)
+{
+    await init(concurso_atual + step)
+}
 const jogos = [
     ["03", "04", "06", "07", "09", "10", "11", "13", "14", "17", "18", "20", "21", "23", "24"],
     ["01", "02", "03", "05", "07", "08", "10", "12", "13", "15", "16", "18", "22", "24", "25"],
